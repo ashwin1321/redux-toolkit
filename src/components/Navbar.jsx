d0e59e7728a713ef6f8e02d428f794aee46b0e9c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-03-02 11:22:34 +0545

    cart items data added to store

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,19 @@ const Navbar = () => {
   const { amount } = useSelector((store) => store.cart);
 
   return (
-    <nav>
-      <div className="nav-center">
-        <h3>Navbar</h3>
-        <div className="nav-container">
-          <CartIcon />
-          <div className="amount-container">
-            <span className="nav-cart-amount">{amount}</span>
+    <>
+      <nav>
+        <div className="nav-center">
+          <h3>Navbar</h3>
+          <div className="nav-container">
+            <CartIcon />
+            <div className="amount-container">
+              <span className="nav-cart-amount">{amount}</span>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
