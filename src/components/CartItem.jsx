df329a9bbae0de1647400d09c43b10d13669e75b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-03-08 12:20:07 +0545

    adding cart items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { ChevronDown, ChevronUp } from '../icons'
 
-const CartItem = () => {
+const CartItem = ({ id, img, title, price, amount }) => {
     return (
 
-        <div>CartItem</div>
+        <article className="cart-item">
+            <img src={img} alt={title} />
+            <div>
+                <h4>{title}</h4>
+                <h5 className="item-price">${price}</h5>
+                {/* remove button */}
+                <button className="remove-btn" type="button">remove</button>
+            </div>
+            <div>
+                {/* increase amount */}
+                <button className="amount-btn" type="button">
+                    <ChevronUp />
+                </button>
+                <p className="amount">{amount}</p>
+                {/* decrease amount */}
+                <button className="amount-btn" type="button">
+                    <ChevronDown />
+                </button>
+            </div>
+
+        </article>
     )
 }
 
